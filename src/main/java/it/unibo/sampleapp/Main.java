1237b7912bd3916d637bdbd71527a319b5f6bb1c
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-03-26 17:51:44 +0100

    Refactor

--- src/main/java/it/unibo/sampleapp/Main.java
+++ src/main/java/it/unibo/sampleapp/Main.java
@@ -1,10 +1,15 @@
 package it.unibo.sampleapp;
 
-
+/**
+ * Entry Point.
+ */
 public final class Main {
 
     private Main() { }
 
+    /**
+     * @param args args
+     */
     public static void main(final String[] args) {
 
     }
