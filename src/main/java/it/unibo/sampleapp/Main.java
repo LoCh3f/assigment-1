ac7b0ac8b1693a60446e8de79a58e1d490bb29a0
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-03-26 17:42:53 +0100

    Removed Useless baseline

---
+++ src/main/java/it/unibo/sampleapp/Main.java
@@ -0,0 +1,18 @@
+package it.unibo.sampleapp;
+
+/**
+ * Monolitic application that fetches movie rates.
+ */
+public final class Main {
+
+    private Main() { }
+
+    /**
+     * Launches the application. Expects {@code OMDB_API_KEY} to be a valid environment variable.
+     *
+     * @param args a string with the movie/series name.
+     */
+    public static void main(final String[] args) {
+
+    }
+}
