f78b794d7d0c594ad7170c42cc6e148a65953ce9
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-03-28 16:18:13 +0100

    Completed test for vector 2D

--- src/test/java/it/unibo/sampleapp/util/Vector2DTest.java
+++ src/test/java/it/unibo/sampleapp/util/Vector2DTest.java
@@ -18,21 +18,43 @@ class Vector2DTest {
 
     @Test
     void add() {
+        Vector2D other = new Vector2D(3, 4);
+        Vector2D result = vector2D.add(other);
+        assertEquals(4.0, result.getX(), EPSILON);
+        assertEquals(6.0, result.getY(), EPSILON);
     }
 
     @Test
     void scale() {
+        Vector2D result = vector2D.scale(2.0);
+        assertEquals(2.0, result.getX(), EPSILON);
+        assertEquals(4.0, result.getY(), EPSILON);
     }
 
     @Test
     void magnitude() {
+        double result = vector2D.magnitude();
+        assertEquals(Math.sqrt(1 + 4), result, EPSILON);
     }
 
     @Test
     void normalize() {
+        Vector2D result = vector2D.normalize();
+        double mag = vector2D.magnitude();
+        assertEquals(1.0 / mag, result.getX(), EPSILON);
+        assertEquals(2.0 / mag, result.getY(), EPSILON);
+
+        // Test zero vector
+        Vector2D zero = new Vector2D(0, 0);
+        Vector2D zeroNormalized = zero.normalize();
+        assertEquals(0.0, zeroNormalized.getX(), EPSILON);
+        assertEquals(0.0, zeroNormalized.getY(), EPSILON);
     }
 
     @Test
     void dot() {
+        Vector2D other = new Vector2D(3, 4);
+        double result = vector2D.dot(other);
+        assertEquals(1 * 3 + 2 * 4, result, EPSILON);
     }
 }
