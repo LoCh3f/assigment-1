104af13d7bb01b2480b26602f4dac3888a62b10b
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-03-28 17:49:27 +0100

    Implemented Hole

---
+++ src/main/java/it/unibo/sampleapp/model/hole/impl/HoleImpl.java
@@ -0,0 +1,33 @@
+package it.unibo.sampleapp.model.hole.impl;
+
+import it.unibo.sampleapp.model.hole.Hole;
+import it.unibo.sampleapp.util.Vector2D;
+
+/**
+ * Implementation of the Hole interface.
+ */
+public final class HoleImpl implements Hole {
+    private final Vector2D position;
+    private final double radius;
+
+    /**
+     * Constructs a new HoleImpl with the given position and radius.
+     *
+     * @param position the position of the hole
+     * @param radius the radius of the hole
+     */
+    public HoleImpl(final Vector2D position, final double radius) {
+        this.position = position;
+        this.radius = radius;
+    }
+
+    @Override
+    public Vector2D getPosition2D() {
+        return this.position;
+    }
+
+    @Override
+    public double getRadius() {
+        return this.radius;
+    }
+}
