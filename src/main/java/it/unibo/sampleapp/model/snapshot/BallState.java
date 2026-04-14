08c842a32994c82869b112f0cce4ad13628dd239
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-04-14 16:43:23 +0200

    Refactor: removed unused record for ball

--- src/main/java/it/unibo/sampleapp/model/snapshot/BallState.java
+++
@@ -1,5 +0,0 @@
-package it.unibo.sampleapp.model.snapshot;
-
-import it.unibo.sampleapp.model.ball.Ball;
-
-public record BallState(int id, double px, double py, double vx, double vy, double radius,Ball.Type lastHit, Ball.Type type) {}
--- src/main/java/it/unibo/sampleapp/model/snapshot/BallUpdate.java
+++
@@ -1,5 +0,0 @@
-package it.unibo.sampleapp.model.snapshot;
-
-import it.unibo.sampleapp.model.ball.Ball;
-
-public record BallUpdate(int id, double newVx, double newVy, double newPx, double newPy, Ball.Type lastHit) {}
